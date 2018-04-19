52ba72f479ef7ffb88f26b920664940a1aed5f66
Pablo Rozas Larraondo <pablo.larraondo@anu.edu.au>
2018-04-19 12:55:42 +1000

    Create unet.py

---
+++ unet.py
@@ -0,0 +1,111 @@
+from keras.models import Sequential
+from keras import layers
+from keras import models
+from keras.layers import BatchNormalization, Conv2D, UpSampling2D, MaxPooling2D, Dropout
+from keras.optimizers import Adam,SGD
+from keras import regularizers
+import numpy as np
+import pickle
+
+def get_unet():
+    concat_axis = 3
+    inputs = layers.Input(shape = (80, 120, 3))
+
+    bn0 = BatchNormalization(axis=3)(inputs)
+    conv1 = layers.Conv2D(32, (3, 3), activation='relu', padding='same', name='conv1_1')(bn0)
+    bn1 = BatchNormalization(axis=3)(conv1)
+    conv1 = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(bn1)
+    bn2 = BatchNormalization(axis=3)(conv1)
+    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(bn2)
+    conv2 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    bn3 = BatchNormalization(axis=3)(conv2)
+    conv2 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(bn3)
+    bn4 = BatchNormalization(axis=3)(conv2)
+    pool2 = layers.MaxPooling2D(pool_size=(2, 2))(bn4)
+
+    conv3 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    bn5 = BatchNormalization(axis=3)(conv3)
+    conv3 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(bn5)
+    bn6 = BatchNormalization(axis=3)(conv3)
+    pool3 = layers.MaxPooling2D(pool_size=(2, 2))(bn6)
+
+    conv4 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    bn7 = BatchNormalization(axis=3)(conv4)
+    conv4 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(bn7)
+    bn8 = BatchNormalization(axis=3)(conv4)
+    pool4 = layers.MaxPooling2D(pool_size=(2, 3))(bn8)
+
+    conv5 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    bn9 = BatchNormalization(axis=3)(conv5)
+    conv5 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(bn9)
+    bn10 = BatchNormalization(axis=3)(conv5)
+
+    up_conv5 = layers.UpSampling2D(size=(2, 3))(bn10)
+    up6 = layers.concatenate([up_conv5, conv4], axis=concat_axis)
+    conv6 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    bn11 = BatchNormalization(axis=3)(conv6)
+    conv6 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(bn11)
+    bn12 = BatchNormalization(axis=3)(conv6)
+
+    up_conv6 = layers.UpSampling2D(size=(2, 2))(bn12)
+    up7 = layers.concatenate([up_conv6, conv3], axis=concat_axis)
+    conv7 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    bn13 = BatchNormalization(axis=3)(conv7)
+    conv7 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(bn13)
+    bn14 = BatchNormalization(axis=3)(conv7)
+
+    up_conv7 = layers.UpSampling2D(size=(2, 2))(bn14)
+    up8 = layers.concatenate([up_conv7, conv2], axis=concat_axis)
+    conv8 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    bn15 = BatchNormalization(axis=3)(conv8)
+    conv8 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(bn15)
+    bn16 = BatchNormalization(axis=3)(conv8)
+
+    up_conv8 = layers.UpSampling2D(size=(2, 2))(bn16)
+    up9 = layers.concatenate([up_conv8, conv1], axis=concat_axis)
+    conv9 = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    bn17 = BatchNormalization(axis=3)(conv9)
+    conv9 = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(bn17)
+    bn18 = BatchNormalization(axis=3)(conv9)
+
+    conv10 = layers.Conv2D(1, (1, 1))(bn18)
+    #bn19 = BatchNormalization(axis=3)(conv10)
+
+    model = models.Model(inputs=inputs, outputs=conv10)
+
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='mae', optimizer=sgd, metrics=['mse'])
+    #model.compile(loss='mae', optimizer=Adam(lr=0.001), metrics=['mse'])
+    print model.summary()
+
+    return model
+
+
+#x = np.load("/datasets/1980-2016/z_1980_2016.npy")
+x = np.load("/datasets/10zlevels.npy")
+y = 1000*np.expand_dims(np.load("/datasets/1980-2016/full_tp_1980_2016.npy"), axis=3)
+
+idxs = np.arange(x.shape[0])
+np.random.seed(0)
+np.random.shuffle(idxs)
+
+x = x[idxs, :, :, :]
+y = y[idxs, :]
+
+y_train = y[:40000, :]
+y_test = y[40000:, :]
+
+# Levels [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100]
+
+i = 0
+j = 2
+k = 6
+x_train = x[:40000, :, :, [i,j,k]]
+x_test = x[40000:, :, :, [i,j,k]]
+
+model = get_unet()
+history = model.fit(x_train, y_train, epochs=50, verbose=1, validation_data=(x_test, y_test))
+with open('trainHistoryDict_unet1_{}-{}-{}'.format(i, j, k), 'wb') as file_pi:
+    pickle.dump(history.history, file_pi)
+
+model.save('/datasets/unet1_{}-{}-{}_.h5')
